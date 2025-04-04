a95be261052541926e01c5caa0db672fd0a6a6aa
Bruno Nunes Garcia <109298177+bnunesgarcia@users.noreply.github.com>
2025-04-03 22:49:30 -0300

    Executar gradualmente 100 usuarios por minuto

--- load-test.js
+++ load-test.js
@@ -6,22 +6,33 @@ export let errorRate = new Rate('errors');
 
 export let options = {
     stages: [
-        { duration: '1m', target: 500 }, 
+        { duration: '1m', target: 100 }, 
+        { duration: '1m', target: 200 },
+        { duration: '1m', target: 300 },
+        { duration: '1m', target: 400 }, 
         { duration: '1m', target: 500 }, 
         { duration: '1m', target: 0 }, 
     ],
     thresholds: {
         errors: ['rate<0.1'], 
-        'http_req_duration': ['p(85)<500'], 
+        'http_req_duration': ['p(95)<500'], 
     },
 };
 
 export default function () {
-    // api publica
-    let response = http.get('https://reqres.in/'); 
-    check(response, {
-        'status was 200': (r) => r.status === 200,
-    }) || errorRate.add(1);
+    let maxRetries = 5;
+    let url = 'https://reqres.in/api/users';
+    let params = { timeout: '60s' };
+    
+    for (let i = 0; i < maxRetries; i++) {
+        let response = http.get(url, params);
+        if (check(response, { 'status was 200': (r) => r.status === 200 })) {
+            break;
+        } else if (i === maxRetries - 1) {
+            errorRate.add(1);
+        }
+        sleep(Math.pow(2, i));
+    }
 
     sleep(1);
 }
